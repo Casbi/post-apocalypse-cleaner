1101c1d26497e30fe505c24f2149b101138dd741
Casbi <yuanyue673@gmail.com>
2019-05-24 15:15:43 +1200

    moved the zombie events to the zombie class

--- src/scripts/myGame.js
+++ src/scripts/myGame.js
@@ -22,8 +22,68 @@ var game = new Phaser.Game(config);
 var myScene;
 
 class Zombie extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, myTextureKey) {
-      super(scene, x, y, myTextureKey);
+    constructor(pScene, x, y, myTextureKey) {
+      super(pScene, x, y, myTextureKey);
+
+      this.play('zombieRun');
+
+    /*  create an empty rectangle game object, probably doesn't have to be circle
+        i just can't find a empty game object
+        immediantly set its *body* to a circle to be used as zombie's vision (check overlap with player) */
+    this.vision = pScene.add.rectangle(this.x, this.y, 0, 0);
+    pScene.physics.add.existing(this.vision);
+    this.vision.body.setCircle(60, -60, -60);
+    this.vision.playerLastPosition = new Phaser.Math.Vector2();
+
+    // check if zombie can see duck and move towards duck if it can
+    pScene.time.addEvent({
+        delay: 100,
+        loop: true,
+        callback: () => {
+            if (pScene.physics.overlap(myGlobal.duck, this.vision)){
+                this.vision.body.debugBodyColor = 0xff9900;
+                pScene.physics.moveTo(this, myGlobal.duck.x, myGlobal.duck.y);
+            } else {
+                this.vision.body.debugBodyColor = 0x0099ff;
+            }
+        }
+    });
+
+    // stop if zombie hasn't seen duck for a while
+    pScene.time.addEvent({
+        delay: 2500,
+        loop: true,
+        callback: () => {
+            if (!pScene.physics.overlap(myGlobal.duck, this.vision)){
+                this.vision.body.debugBodyColor = 0x0099ff;
+                this.body.stop();
+            } else {
+                this.vision.body.debugBodyColor = 0xff9900;
+            }
+        }
+    });
+
+    // stop if zombie reached duck
+    pScene.time.addEvent({
+        delay: 17,
+        loop: true,
+        callback: () => {
+            if (pScene.physics.overlap(myGlobal.duck, this)) {
+                this.body.stop();
+            }
+        }
+    });
+
+    // keep zombie vision on the zombie if the zombie moves
+    pScene.time.addEvent({
+        loop: true,
+        callback: () => {
+            if (this.body.moves) {
+                this.vision.x = this.x;
+                this.vision.y = this.y;
+            }
+        }
+    });
 
     }
 }
@@ -80,67 +140,10 @@ function create() {
             }
         }
     });
-    
 
-    myGlobal.zombie = myScene.add.existing(new Zombie(myScene, 200, 100, 'charactersWeapons')).play('zombieRun');
+    // add zombie to the scene (both the display list and the physics manager)
+    myGlobal.zombie = myScene.add.existing(new Zombie(myScene, 200, 100, 'charactersWeapons'));
     myScene.physics.add.existing(myGlobal.zombie);
-
-    // myGlobal.zombie = myScene.physics.add.sprite(200, 100, 'charactersWeapons').play('zombieRun');
-
-    /*  create an empty rectangle game object, probably doesn't have to be circle
-        i just can't find a empty game object
-        immediantly set its *body* to a circle to be used as zombie's vision (check overlap with player) */
-    myGlobal.zombie.vision = myScene.add.rectangle(myGlobal.zombie.x, myGlobal.zombie.y, 0, 0);
-    myScene.physics.add.existing(myGlobal.zombie.vision);
-    myGlobal.zombie.vision.body.setCircle(60, -60, -60);
-    myGlobal.zombie.vision.playerLastPosition = new Phaser.Math.Vector2();
-
-    myScene.time.addEvent({
-        delay: 100,
-        loop: true,
-        callback: function(){
-            if (myScene.physics.overlap(myGlobal.duck, myGlobal.zombie.vision)){
-                myGlobal.zombie.vision.body.debugBodyColor = 0xff9900;
-                myScene.physics.moveTo(myGlobal.zombie, myGlobal.duck.x, myGlobal.duck.y);
-            } else {
-                myGlobal.zombie.vision.body.debugBodyColor = 0x0099ff;
-            }
-        }
-    });
-
-    myScene.time.addEvent({
-        delay: 2500,
-        loop: true,
-        callback: function(){
-            if (!myScene.physics.overlap(myGlobal.duck, myGlobal.zombie.vision)){
-                myGlobal.zombie.vision.body.debugBodyColor = 0x0099ff;
-                myGlobal.zombie.body.stop();
-            } else {
-                myGlobal.zombie.vision.body.debugBodyColor = 0xff9900;
-            }
-        }
-    });
-
-    myScene.time.addEvent({
-        delay: 17,
-        loop: true,
-        callback: function(){
-            if (myScene.physics.overlap(myGlobal.duck, myGlobal.zombie)) {
-                myGlobal.zombie.body.stop();
-            }
-        }
-    });
-
-    // keep zombie vision on the zombie if the zombie moves
-    myScene.time.addEvent({
-        loop: true,
-        callback: function() {
-            if (myGlobal.zombie.body.moves) {
-                myGlobal.zombie.vision.x = myGlobal.zombie.x;
-                myGlobal.zombie.vision.y = myGlobal.zombie.y;
-            }
-        }
-    });
 
 }
 
